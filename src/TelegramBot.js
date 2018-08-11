7fffbf67709b2c2c6c9192a44fdf75d2d76689d8
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2018-08-11 23:42:52 +0300

    Add inline query support

--- src/TelegramBot.js
+++ src/TelegramBot.js
@@ -1,43 +1,49 @@
-var TelegramBot = require('node-telegram-bot-api');
+var TelegramBot = require("node-telegram-bot-api");
 
-exports._connect = function (options, token) {
-  return function () {
+exports._connect = function(options, token) {
+  return function() {
     return new TelegramBot(token, options);
   };
 };
 
 exports._sendMessage = function(bot, id, message, options) {
-  return function () {
+  return function() {
     bot.sendMessage(id, message, options);
   };
 };
 
+exports._answerInlineQuery = function(bot, id, results, options) {
+  return function() {
+    bot.answerInlineQuery(id, results, options);
+  };
+};
+
 exports._onMessage = function(bot, eff) {
-  return function () {
-    bot.on('message', function (msg) {
+  return function() {
+    bot.on("message", function(msg) {
       eff(msg)();
     });
   };
 };
 
-exports._onText = function (bot, regex, eff) {
-  return function () {
-    bot.onText(regex, function (msg, match) {
+exports._onText = function(bot, regex, eff) {
+  return function() {
+    bot.onText(regex, function(msg, match) {
       eff(msg)(match)();
     });
   };
 };
 
-exports._getMe = function (bot) {
-  return function () {
+exports._getMe = function(bot) {
+  return function() {
     return bot.getMe();
   };
 };
 
-exports.runPromise = function (err) {
-  return function (succ) {
-    return function (promise) {
-      return function () {
+exports.runPromise = function(err) {
+  return function(succ) {
+    return function(promise) {
+      return function() {
         promise.then(succ).catch(err);
       };
     };
--- src/TelegramBot.purs
+++ src/TelegramBot.purs
@@ -14,15 +14,16 @@ import Effect.Uncurried (EffectFn1, mkEffectFn1)
 import Foreign (F, Foreign)
 import Simple.JSON (read')
 
---| The Telegram Bot API Token.
+-- | The Telegram `Bot` API Token.
 type Token = String
 
---| The Telegram Bot options.
+-- | The Telegram `Bot` options.
 type Options =
   { polling :: Boolean
   }
 
---| The Telegram Message type. See https://core.telegram.org/bots/api#message
+-- | The Telegram `Message` type.
+-- | See https://core.telegram.org/bots/api#message
 type Message =
   { message_id :: Int
   , from :: Maybe User
@@ -32,7 +33,38 @@ type Message =
   , text :: Maybe String
   }
 
---| The Telegram User type. See https://core.telegram.org/bots/api#user
+-- | The Telegram `InlineQuery` type.
+-- | See https://core.telegram.org/bots/api#inlinequery
+type InlineQuery =
+  { id :: String
+  , from :: Maybe User
+  , location :: Maybe Location
+  , query :: String
+  , offset :: Int
+  }
+
+-- | The Telegram `InlineQueryResultDocument` type.
+-- | See https://core.telegram.org/bots/api#inlinequeryresultdocument
+type InlineQueryResultDocument =
+  { type :: String
+  , id :: String
+  , title :: String
+  -- , caption :: Maybe String
+  -- , description :: Maybe String
+  , input_message_content :: InputTextMessageContent
+  , parse_mode :: String
+  }
+
+-- | The Telegram `InputTextMessageContent` type.
+-- | See https://core.telegram.org/bots/api#inputtextmessagecontent
+type InputTextMessageContent =
+  { message_text :: String
+  , parse_mode :: String
+  , disable_web_page_preview :: Boolean
+  }
+
+-- | The Telegram `User` type.
+-- | See https://core.telegram.org/bots/api#user
 type User =
   { id :: Int
   , first_name :: Maybe String
@@ -40,7 +72,8 @@ type User =
   , username :: Maybe String
   }
 
---| The Telegram Chat type. See https://core.telegram.org/bots/api#chat
+-- | The Telegram `Chat` type.
+-- | See https://core.telegram.org/bots/api#chat
 type Chat =
   { id :: Int
   , type :: String
@@ -49,13 +82,15 @@ type Chat =
   , username :: Maybe String
   }
 
---| The Telegram Location type. See https://core.telegram.org/bots/api#location
+-- | The Telegram `Location` type.
+-- | See https://core.telegram.org/bots/api#location
 type Location =
   { longitude :: Number
   , latitude :: Number
   }
 
---| The Regex execution matches. See https://github.com/yagop/node-telegram-bot-api#TelegramBot+onText
+-- | The `Regex` execution matches.
+-- | See https://github.com/yagop/node-telegram-bot-api#TelegramBot+onText
 type Matches = Maybe (Array String)
 
 foreign import data Bot :: Type
@@ -65,14 +100,9 @@ defaultOptions =
   { polling: true
   }
 
-foreign import _connect ::
-  Fn2
-    Options
-    Token
-    (Effect Bot)
-connect ::
-  Token ->
-  Effect Bot
+foreign import _connect :: Fn2 Options Token (Effect Bot)
+
+connect :: Token -> Effect Bot
 connect = runFn2 _connect defaultOptions
 
 foreign import _sendMessage ::
@@ -82,14 +112,32 @@ foreign import _sendMessage ::
     String
     Foreign
     (Effect Unit)
+
 sendMessage ::
   Bot ->
   Int ->
   String ->
   Foreign ->
   Effect Unit
-sendMessage bot id message options = do
+sendMessage bot id message options =
   runFn4 _sendMessage bot id message options
+
+foreign import _answerInlineQuery ::
+  Fn4
+    Bot
+    Int
+    (Array InlineQueryResultDocument)
+    Foreign
+    (Effect Unit)
+
+answerInlineQuery ::
+  Bot ->
+  Int ->
+  (Array InlineQueryResultDocument) ->
+  Foreign ->
+  Effect Unit
+answerInlineQuery bot id results options =
+  runFn4 _answerInlineQuery bot id results options
 
 foreign import _onText ::
   Fn3
@@ -97,25 +145,27 @@ foreign import _onText ::
     Regex
     (Foreign -> Foreign -> Effect Unit)
     (Effect Unit)
---| For adding a callback for on text matching a regex pattern.
+
+-- | For adding a callback for on text matching a regex pattern.
 onText ::
   Bot ->
   Regex ->
   (F Message -> F Matches -> Effect Unit) ->
   (Effect Unit)
-onText bot regex handler = do
+onText bot regex handler =
   onText' bot regex handleMessage
   where
     handleMessage foreignMsg foreignMatches =
       handler (read' foreignMsg) (read' foreignMatches)
 
---| For getting the Foreign values directly. The callback is Message -> Matches -> Effect _ Unit.
+-- | For getting the `Foreign` values directly.
+-- | The callback is `Message -> Matches -> Effect _ Unit`.
 onText' ::
   Bot ->
   Regex ->
   (Foreign -> Foreign -> Effect Unit) ->
   (Effect Unit)
-onText' bot regex handler = do
+onText' bot regex handler =
   runFn3 _onText bot regex handler
 
 foreign import _onMessage ::
@@ -124,20 +174,14 @@ foreign import _onMessage ::
     (Foreign -> Effect Unit)
     (Effect Unit)
 
---| For adding a callback for all messages.
-onMessage ::
-  Bot ->
-  (F Message -> Effect Unit) ->
-  (Effect Unit)
-onMessage bot handler = do
+-- | For adding a callback for all messages.
+onMessage :: Bot -> (F Message -> Effect Unit) -> (Effect Unit)
+onMessage bot handler =
   onMessage' bot $ handler <<< read'
 
---| For getting the Foreign value directly from onMessage
-onMessage' ::
-  Bot ->
-  (Foreign -> Effect Unit) ->
-  (Effect Unit)
-onMessage' bot handler = do
+-- | For getting the `Foreign` value directly from onMessage.
+onMessage' :: Bot -> (Foreign -> Effect Unit) -> (Effect Unit)
+onMessage' bot handler =
   runFn2 _onMessage bot handler
 
 foreign import data Promise :: Type -> Type
@@ -147,14 +191,10 @@ foreign import runPromise :: forall a
   -> Promise a
   -> Effect Unit
 
-foreign import _getMe ::
-  Fn1
-    Bot
-    (Effect (Promise Foreign))
---| For adding a callback for all messages.
-getMe ::
-  Bot ->
-  (Aff (F User))
+foreign import _getMe :: Fn1 Bot (Effect (Promise Foreign))
+
+-- | For adding a callback for all messages.
+getMe :: Bot -> (Aff (F User))
 getMe bot = do
   p <- liftEffect $ runFn1 _getMe bot
   read' <$> makeAff
